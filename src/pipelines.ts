546df3e316d244f4eeba4044301f336437b835cc
Michael Orenstein <mike@whiteshadows.me>
2018-10-07 00:35:53 +1000

    chore: Cleanup

--- src/pipelines.ts
+++ src/pipelines.ts
@@ -2,6 +2,8 @@ import { DuplexPipeline, Pipeline } from '@electricui/core'
 
 import { decode, encode, nullByteBuffer } from './cobs'
 
+const dPipelines = require('debug')('electricui-protocol-binary-cobs:pipelines')
+
 export class COBSEncoderPipeline extends Pipeline {
   receive(packet: Buffer) {
     return this.push(encode(packet))
@@ -14,6 +16,8 @@ export class COBSDecoderPipeline extends Pipeline {
   receive(packet: Buffer) {
     const promises: Array<Promise<any>> = []
 
+    dPipelines(`Received data to cobs decode`, packet)
+
     let data = Buffer.concat([this.buffer, packet])
     let position
 
@@ -21,12 +25,17 @@ export class COBSDecoderPipeline extends Pipeline {
       const framed = data.slice(0, position)
 
       if (framed.length > 0) {
+        dPipelines(
+          `...Found a delimiter, pushing a chunk up to the binary decoder`,
+        )
+
         promises.push(this.push(decode(framed)))
       }
 
       data = data.slice(position + 1)
     }
     this.buffer = data
+    dPipelines(`buffer leftover: `, this.buffer)
 
     return Promise.all(promises)
   }
