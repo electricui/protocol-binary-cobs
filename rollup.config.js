34375f8a3d03b3b4ca88afd8227fc0a16105e5df
Michael Orenstein <michael@whiteshadows.me>
2020-02-15 14:58:00 +1030

    chore: Build chain cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -1,44 +1,8 @@
-import autoExternal from 'rollup-plugin-auto-external'
-import copy from 'rollup-plugin-copy'
-import license from 'rollup-plugin-license'
-import path from 'path'
-import typescript from 'rollup-plugin-typescript2'
+const config = require('@electricui/build-rollup-config/rollup.config.js')
 
 export default async () => {
-  return {
-    input: 'index.ts',
-    output: [
-      { format: 'cjs', file: path.resolve(__dirname, 'lib', `index.js`) },
-    ],
-    plugins: [
-      autoExternal({
-        builtins: true,
-        dependencies: true,
-        packagePath: path.resolve(__dirname, 'package.json'),
-        peerDependencies: true,
-      }),
-      typescript({
-        objectHashIgnoreUnknownHack: true, // Using the copy plugin (which uses async functions), we need to invalidate the cache on every build
-        clean: true,
-      }),
-      license({
-        sourcemap: true,
-        banner: {
-          commentStyle: 'ignored',
-          content: {
-            file: path.join(__dirname, '..', '..', 'LICENCE_TEMPLATE'),
-          },
-        },
-      }),
-      copy({
-        targets: [
-          {
-            src: path.join(__dirname, '..', '..', 'LICENCE_TEMPLATE'),
-            dest: '.',
-            rename: () => 'LICENCE',
-          },
-        ],
-      }),
-    ],
-  }
+  // Import our default rollup config.
+  const configOptions = {}
+
+  return config(__dirname, configOptions)
 }
